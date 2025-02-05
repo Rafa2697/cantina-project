f3474b2e6cf46740091c5dc8532c281c9b81cf83
Rafael <Rafael.mattos468@gmail.com>
2025-02-05 19:42:40 -0300

    Adiciona rotas para gerenciamento de alimentos: GET, POST e DELETE

---
+++ src/app/api/auth/cadastrarAlimento/route.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+
+export async function GET(){
+    const alimento = await prisma.menuItem.findMany()
+
+    return Response.json(alimento)
+}
+
+export async function POST(request: Request){
+    try{
+        const data = await request.json()
+        const {name, description, price, categoryId, isAvailable} = data;
+        const newAlimento = await prisma.menuItem.create({
+            data:{
+                name,
+                description,
+                price,
+                categoryId,
+                isAvailable
+            }
+        })
+
+        return new Response('Sucess!', {status:200})
+    }catch(error){
+        console.error(error);
+        return new Response(JSON.stringify({ error: 'Erro ao inserir alimento' }), { status: 500 });
+    }
+}
+
+export async function DELETE(request:Request) {
+    try{
+        const {id} = await request.json()
+        const alimento = await prisma.menuItem.delete({
+            where:{
+                id
+            }
+        })
+
+        return NextResponse.json(alimento);
+    }catch( error){
+        console.error(error)
+       return new Response(JSON.stringify({error:'ERRO ao deletar alemento' }))
+    }
+    
+}
