8ab1dfb740528d49cb13117614d84167be3d9cc6
Rafael <Rafael.mattos468@gmail.com>
2025-02-14 20:52:32 -0300

    Iniciando a criação do componente de pedidos do administrador

--- src/app/components/OrdersReceived.tsx
+++ src/app/components/OrdersReceived.tsx
@@ -1,43 +1,73 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 
-// interface DataItem {
-//     id: string;
-//     userName: string;
-//     userEmail: string;
-//     status: string;
-//     totalPrice: string;
-//     items: [
-//         {
-//             id:string;
-//             orderId: string;
-//             foodId: string;
-//             name: string;
-//             quantity: number;
-//             subtotal:string
-//         }
-//     ];
-// }
+interface DataItem {
+    id: string;
+    userName: string;
+    userEmail: string;
+    status: string;
+    totalPrice: string;
+    items: [
+        {
+            id: string;
+            orderId: string;
+            foodId: string;
+            name: string;
+            quantity: number;
+            subtotal: string
+        }
+    ];
+}
 
 export default function OrdersReceived() {
-    // const [data, setData] = useState<DataItem[]>([]);
-    // const [loading, setLoading] = useState(false)
+    const [data, setData] = useState<DataItem[]>([]);
+    const [loading, setLoading] = useState(false)
 
-    // const fetchData = async () => {
-    //     setLoading(true)
-    //     try {
-    //         const response = await fetch("/api/items"); // Atualize o endpoint conforme necessário
-    //         const result: DataItem[] = await response.json();
-    //         setData(result);
-    //     } catch (error) {
-    //         console.error('Error ao buscar dados: ', error)
-    //     }
-    //     setLoading(false)
-    // }
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
+    const fetchData = async () => {
+        setLoading(true)
+        try {
+            const response = await fetch("/api/items"); // Atualize o endpoint conforme necessário
+            const result: DataItem[] = await response.json();
+            setData(result);
+        } catch (error) {
+            console.error('Error ao buscar dados: ', error)
+        }
+        setLoading(false)
+    }
+    useEffect(() => {
+        fetchData();
+    }, []);
     return (
-        <h1>Pedidos recebidos para o administrador</h1>
+        <div>
+            {loading ? (
+                <div>
+                    <p>carregando...</p>
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {data.map((order) => (
+                        <div key={order.id} className="bg-white rounded-lg shadow-md p-4">
+                            <p className="text-gray-600 mb-2">Cliente:{order.userName}</p>
+                            <p className="text-gray-600 mb-4">Identificação:{order.userEmail}</p>
+                            <p className="text-gray-600 mb-4">status:{order.status}</p>
+                            <div className="space-y-2">
+                                {order.items.map((item) => (
+                                    <div key={item.id} className="border-b pb-2">
+                                        <p className="font-medium">{item.name}</p>
+                                        <p className="text-sm text-gray-500">
+                                            Quantidade: {item.quantity} | Subtotal: R$ {item.subtotal}
+                                        </p>
+                                    </div>
+                                ))}
+                                <p className="mt-4 text-lg font-bold">
+                                    Total: R$ {order.totalPrice}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+            )}
+        </div>
     )
 }
