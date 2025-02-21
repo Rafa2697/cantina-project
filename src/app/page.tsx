6186df872932dd50d79117c6fe4b0fdfc4baef92
Rafael <Rafael.mattos468@gmail.com>
2025-02-21 20:00:44 -0300

    Atualiza layout da página inicial com novas imagens de fundo e altera cores dos botões de login e cardápio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,14 @@ import { LogIn } from 'lucide-react';
 
 export default function Home() {
     return (
-        <div className="container flex flex-col justify-around items-center h-svh">
-            <h1 className="text-center py-3 font-bold">Cantina Fasupi</h1>
+        <div className="container min-h-screen bg-fundo-mobile sm:bg-fundo-desktop bg-center bg-no-repeat" >
             <div className="container flex flex-col sm:flex-row justify-around items-center h-svh" >
 
-                <Link href="/signIn" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 text-white font-bold text-xl">
+                <Link href="/signIn" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-sky-400 hover:bg-sky-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-sky-700 text-white font-bold text-xl">
                     Login
                     <LogIn />
                 </Link>
-                <Link href="/pedidos" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 text-white font-bold text-xl">
+                <Link href="/pedidos" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-orange-400 hover:bg-orange-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-orange-700 text-white font-bold text-xl">
                     Cardapio
                 </Link>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ export default {
   ],
   theme: {
   	extend: {
+		backgroundImage: {
+			'fundo-mobile': "url('/fundoMobile.svg')",
+			'fundo-desktop': "url('/fundoDesktop.png')",
+		  },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
