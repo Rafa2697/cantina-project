8ff76ec88228a75b372d9bc9467f241357889b58
Rafael <rafael.mattos468@gmail.com>
2025-03-08 19:39:59 -0300

    Ajusta o layout da página inicial para centralizar os elementos e adiciona link para o site do autor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,20 @@ import { LogIn } from 'lucide-react';
 export default function Home() {
     
     return (
-        <div className="container bg-fundo-mobile sm:bg-fundo-desktop bg-cover bg-center bg-no-repeat">
-            <div className=" flex flex-col sm:flex-row justify-around items-center h-svh " >
-
-                <Link href="/signIn" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-sky-400 hover:bg-sky-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-sky-700 text-white font-bold text-xl">
-                    Login
-                    <LogIn />
-                </Link>
-                <Link href="/pedidos" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-orange-400 hover:bg-orange-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-orange-700 text-white font-bold text-xl">
-                    Cardápio
-                </Link>
+        <div className="container n bg-fundo-mobile sm:bg-fundo-desktop bg-cover bg-center bg-no-repeat">
+            <div className="h-screen flex flex-col justify-center" >
+                <div className=" flex flex-col sm:flex-row justify-around items-center h-5/6">
+                    <Link href="/signIn" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-sky-400 hover:bg-sky-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-sky-700 text-white font-bold text-xl">
+                        Login
+                        <LogIn />
+                    </Link>
+                    <Link href="/pedidos" className="border w-64 items-center justify-center h-1/4 flex gap-1 rounded-md bg-orange-400 hover:bg-orange-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-orange-700 text-white font-bold text-xl">
+                        Cardápio
+                    </Link>
+                </div>
+                <p className="text-center bg-slate-100 w-40 mx-auto rounded-sm hover:text-sky-700">&copy;<a href="https://www.rafaeldev.com/" target="_blank"> rafaeldev.com</a></p>
             </div>
+            
         </div>
     )
 }
